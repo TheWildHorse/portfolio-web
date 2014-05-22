f7f1f69140626356c4dca21eb449c15c545fcd6d
Igor Rinkovec <shiqqqi@gmail.com>
2014-05-22 15:09:50 +0200

    First draft of the website.

--- webroot/js/init.js
+++ webroot/js/init.js
@@ -1,9 +1,22 @@
 ( function( $ ) {
+    // Determine absolute positioned DIV heights
+    $('#handshake').height($('#zrak').height()+150);
+    $('#meetme').height($('#handshake').height()+$('#social').height()+170);
+    //Set the final height and return the brain to its normal size
+    $('#summary').height($('#summary').height()+$('#brain').height()*1.5);
+
     // Init Skrollr
     var s = skrollr.init({
         render: function(data) {
             //Debugging - Log the current scroll position.
-            console.log(data.curTop);
+            //$("#curPos").text(data.curTop);
         }
+    });
+
+    //Back to top button
+    $('#top').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, 1000);
+        return false;
     });
 } )(jQuery);
