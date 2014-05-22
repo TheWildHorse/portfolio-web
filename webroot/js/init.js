4d7301535f8e7ffc87aacd375ae4e2a4b83db744
Igor Rinkovec <shiqqqi@gmail.com>
2014-05-22 15:16:27 +0200

    Added a delay on the init script to prevent first-load bugs.

--- webroot/js/init.js
+++ webroot/js/init.js
@@ -1,22 +1,24 @@
 ( function( $ ) {
-    // Determine absolute positioned DIV heights
-    $('#handshake').height($('#zrak').height()+150);
-    $('#meetme').height($('#handshake').height()+$('#social').height()+170);
-    //Set the final height and return the brain to its normal size
-    $('#summary').height($('#summary').height()+$('#brain').height()*1.5);
+    setTimeout(function() {
+    	// Determine absolute positioned DIV heights
+	    $('#handshake').height($('#zrak').height()+150);
+	    $('#meetme').height($('#handshake').height()+$('#social').height()+170);
+	    //Set the final height and return the brain to its normal size
+	    $('#summary').height($('#summary').height()+$('#brain').height()*1.5);
 
-    // Init Skrollr
-    var s = skrollr.init({
-        render: function(data) {
-            //Debugging - Log the current scroll position.
-            //$("#curPos").text(data.curTop);
-        }
-    });
+	    // Init Skrollr
+		var s = skrollr.init({
+	        render: function(data) {
+	            //Debugging - Log the current scroll position.
+	            //$("#curPos").text(data.curTop);
+	        }
+	    });
 
-    //Back to top button
-    $('#top').click(function(event) {
-        event.preventDefault();
-        $('html, body').animate({scrollTop: 0}, 1000);
-        return false;
-    });
+	    //Back to top button
+	    $('#top').click(function(event) {
+	        event.preventDefault();
+	        $('html, body').animate({scrollTop: 0}, 1000);
+	        return false;
+	    });
+    }, 1000);
 } )(jQuery);
